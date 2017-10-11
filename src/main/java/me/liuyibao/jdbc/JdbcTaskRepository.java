4ec1a7a34fcc15d0963bcea9bb8d0deabfdec101
liuyibao <liuyibao@wanda.cn>
2017-10-11 16:03:58 +0800

    TaskRowMapper改成内部静态类或改成Lambda表达式也可以

--- src/main/java/me/liuyibao/jdbc/JdbcTaskRepository.java
+++ src/main/java/me/liuyibao/jdbc/JdbcTaskRepository.java
@@ -2,7 +2,11 @@ package me.liuyibao.jdbc;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcOperations;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 /**
  * Created by liuyibao on 2017/10/11.
@@ -18,5 +22,16 @@ public class JdbcTaskRepository implements TaskRepository {
 
     public Task findOne(long id) {
         return jdbcOperations.queryForObject("SELECT * FROM task WHERE id = ?", new TaskRowMapper(), id);
+    }
+
+    private static final class TaskRowMapper implements RowMapper<Task> {
+        public Task mapRow(ResultSet rs, int rowNum) throws SQLException {
+            return new Task(
+                    rs.getLong("id"),
+                    rs.getLong("user_id"),
+                    rs.getLong("project_id"),
+                    rs.getString("title")
+            );
+        }
     }
 }
